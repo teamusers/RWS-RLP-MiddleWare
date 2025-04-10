9da31c5a9d6170ca4b79a48f704a98ed34537417
Jeshua <jeshua.woon@user.com.sg>
2025-04-10 23:15:04 +0800

    feature: email service

---
+++ api/http/services/email.go
@@ -0,0 +1,78 @@
+package services
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	config "rlp-middleware/config"
+	models "rlp-middleware/models"
+
+	"gopkg.in/gomail.v2"
+)
+
+type EmailService struct {
+	SmtpConfig *config.SmtpConfig
+	Dialer     *gomail.Dialer
+}
+
+type TemplateData struct {
+	User *models.User
+	OTP  string
+}
+
+func NewEmailService(smtpConfig *config.SmtpConfig) *EmailService {
+	dialer := gomail.NewDialer(
+		smtpConfig.Host,
+		smtpConfig.Port,
+		smtpConfig.User,
+		smtpConfig.Password,
+	)
+
+	return &EmailService{
+		SmtpConfig: smtpConfig,
+		Dialer:     dialer,
+	}
+}
+
+func (es *EmailService) SendEmail(recipient, subject, templateName string, templateData any) error {
+	content, err := es.loadTemplate(templateName, templateData)
+	if err != nil {
+		return fmt.Errorf("failed to load template: %w", err)
+	}
+
+	// Create a new message
+	m := gomail.NewMessage()
+
+	// Set the sender, recipient, subject, and body of the email
+	m.SetHeader("From", es.SmtpConfig.From)
+	m.SetHeader("To", recipient)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", content)
+
+	// Send the email
+	err = es.Dialer.DialAndSend(m)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	log.Printf("Email sent successfully to %s", recipient)
+	return nil
+}
+
+func (es *EmailService) loadTemplate(templateName string, data any) (string, error) {
+	templatePath := fmt.Sprintf("templates/%s", templateName)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse template: %w", err)
+	}
+
+	var tplBuffer bytes.Buffer
+	err = tmpl.Execute(&tplBuffer, data)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute template: %w", err)
+	}
+
+	return tplBuffer.String(), nil
+}
