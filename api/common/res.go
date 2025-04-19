50b9fe227f5237657cb1b9e1a121ac9ce47e000e
eric Gan <eric_gan0513@hotmail.com>
2025-04-19 20:15:43 +0800

    code/files clean up. Add user developer gmail account

--- api/common/res.go
+++
@@ -1,8 +0,0 @@
-package common
-
-type Response struct {
-	Code      int64       `json:"code"`
-	Msg       string      `json:"msg"`
-	Timestamp int64       `json:"timestamp"`
-	Data      interface{} `json:"data"`
-}
