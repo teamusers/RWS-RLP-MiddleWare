a9e0fc3de99871ee269c91e1972b439fe81568a6
eric Gan <eric_gan0513@hotmail.com>
2025-04-22 14:11:10 +0800

    UPDATE SWAGGER message

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 // @version         1.0
 // @description     Endpoints for authentication, login and register
 // @description
-// @description ## 📋 Codes
+// @description <details open>
+// @description   <summary><a href="javascript:void(0)" style="cursor: pointer !important;">📋 Message Codes</a></summary>
+// @description
 // @description | Code   | Description                   |
-// @description | ------ | ----------------------        |
+// @description | ------ | ------------------------------|
 // @description | 1000   | successful                    |
 // @description | 1001   | unsuccessful                  |
 // @description | 1002   | found                         |
@@ -24,6 +26,8 @@ import (
 // @description | 4006   | invalid appid                 |
 // @description | 4007   | missing appid                 |
 // @description | 4008   | invalid query parameters      |
+// @description
+// @description </details>
 // @host            localhost:18080
 // @BasePath        /api/v1
 
