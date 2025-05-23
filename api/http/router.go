50fdb299ea5470f15610dd09bdb0917b923abbfd
Jeshua Woon <jeshua.woon@user.com.sg>
2025-05-23 15:01:30 +0800

    feat: add invalid or missing query param handler

--- api/http/router.go
+++ api/http/router.go
@@ -33,13 +33,17 @@ func Routers(e *gin.RouterGroup) {
 		usersGroup.POST("/gr-cms", user.VerifyGrCmsExistence)
 		//GET - LBE-8 - api/v1/user/gr-reg - verify GR user's profile pushed by CMS
 		usersGroup.GET("/gr-reg/:reg_id", user.GetCachedGrCmsProfile)
+		usersGroup.GET("/gr-reg", v1.InvalidQueryParametersHandler)
 
 		//GET - LBE-9 - api/v1/user/:external_id - get user profile from rlp
 		usersGroup.GET("/:external_id", user.GetUserProfile)
+		usersGroup.GET("", v1.InvalidQueryParametersHandler)
 		//PUT - LBE-10 - api/v1/user/update/:external_id - update user profile
 		usersGroup.PUT("/update/:external_id", user.UpdateUserProfile)
+		usersGroup.PUT("/update", v1.InvalidQueryParametersHandler)
 		//PUT - LBE-11 - api/v1/user/archive - withdraw user profile (active_status=0, previous email=current email, email=null)
 		usersGroup.PUT("/archive/:external_id", user.WithdrawUserProfile)
+		usersGroup.PUT("/archive", v1.InvalidQueryParametersHandler)
 	}
 
 }
